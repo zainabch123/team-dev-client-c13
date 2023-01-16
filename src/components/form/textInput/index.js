196b21c587abf1f7a4f682a96ad6a1b6d0ab3ab5
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-17 02:30:59 +0800

    break up css into files, format textinput js

--- src/components/form/textInput/index.js
+++ src/components/form/textInput/index.js
@@ -1,10 +1,10 @@
-const TextInput = ({ value, onChange, name, label, type = 'text' }) => {
-    return (
-        <>
-            <label htmlFor={name}>{label}</label>
-            <input type={type} name={name} value={value} onChange={onChange} />
-        </>
-    )
-}
+const TextInput = ({ value, onChange, name, label, type = "text" }) => {
+	return (
+		<div>
+			<label htmlFor={name}>{label}</label>
+			<input type={type} name={name} value={value} onChange={onChange} />
+		</div>
+	);
+};
 
-export default TextInput
+export default TextInput;
