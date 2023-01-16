23f926b11df22bb6e070dcb192801cb95f2b53f3
Nathan King <nathanking3590@gmail.com>
2023-01-16 11:17:23 +0000

    use mock API client for component data

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -11,7 +11,15 @@ const Dashboard = () => {
 
     return (
         <>
-            {posts.map(post => <Post key={post.id} name={`${post.author.firstName} ${post.author.lastName}`} date={post.createdAt} content={post.content} comments={post.comments} />)}
+            {posts.map(post => {
+                return <Post
+                    key={post.id}
+                    name={`${post.author.firstName} ${post.author.lastName}`}
+                    date={post.createdAt}
+                    content={post.content}
+                    comments={post.comments}
+                />
+            })}
         </>
     );
 }
