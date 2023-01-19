7800755ad8f4cda7973f11285eb287faf90768e7
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-20 02:51:12 +0800

    setup verification page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Dashboard from "./pages/dashboard";
 import Login from "./pages/login";
 import Register from "./pages/register";
 import Loading from "./pages/loading";
+import Verification from "./pages/verification";
 import { AuthProvider, ProtectedRoute } from "./context/auth";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
 					<Route path="login" element={<Login />} />
 					<Route path="register" element={<Register />} />
 					<Route path="loading" element={<Loading />} />
+					<Route path="verification" element={<Verification />} />
 
 					<Route
 						index
