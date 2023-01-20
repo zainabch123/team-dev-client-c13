14c273e5b2cf0a1419b3b421f16319f7a127a0b1
Nathan King <nathanking3590@gmail.com>
2023-01-20 11:38:14 +0000

    add post styling

--- src/components/post/index.js
+++ src/components/post/index.js
@@ -1,14 +1,42 @@
 import Card from "../card"
 import Comment from "../comment"
+import "./style.css"
 
 const Post = ({ name, date, content, comments = [] }) => {
     return (
         <Card>
-            <h4>{name}</h4>
-            <small>{date}</small>
-            <p>{content}</p>
-            
-            {comments.map(comment => <Comment key={comment.id} name={comment.name} content={comment.content} />)}
+            <article className="post">
+                <section id='post-details'>
+                    <div className="profile-icon"></div>
+
+                    <div>
+                        <p>{name}</p>
+                        <small>{date}</small>
+                    </div>
+                    
+                    <div className="edit-icon">
+                        <p>...</p>
+                    </div>
+                </section>
+
+                <section>
+                    <p>{content}</p>
+                </section>
+
+                <section className="post-interactions-container">
+                    <div className="post-interactions">
+                        <div>Like</div>
+                        <div>Comment</div>
+                    </div>
+
+                    <p>Be the first to like this</p>
+                    
+                </section>
+
+                <section>
+                    {comments.map(comment => <Comment key={comment.id} name={comment.name} content={comment.content} />)}
+                </section>
+            </article>
         </Card>
     )
 }
