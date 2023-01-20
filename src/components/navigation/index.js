d55920254549127da7d4e2f01bd4fe17a7a91fd0
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-20 23:34:03 +0800

    add disablesnav option for navigation, linting

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,26 +1,32 @@
-import { NavLink } from "react-router-dom"
-import useAuth from "../../hooks/useAuth"
-import Button from "../button"
-import './style.css'
+import { NavLink } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import Button from "../button";
+import "./style.css";
 
-const Navigation = () => {
-    const { token, onLogout } = useAuth()
+const Navigation = ({ disabledNav }) => {
+	const { token, onLogout } = useAuth();
 
-    if (!token) {
-        return null
-    }
+	if (!token) {
+		return null;
+	}
 
-    return (
-        <nav>
-            <ul>
-                <li><NavLink to='/'>Dashboard</NavLink></li>
-                <li><NavLink to='/login'>Login</NavLink></li>
-                <li><NavLink to='/register'>Register</NavLink></li>
-            </ul>
-            
-            {token && <Button text='Logout' onClick={onLogout} />}
-        </nav>
-    )
-}
+	return (
+		<nav>
+			<ul>
+				<li>
+					<NavLink to="/">Dashboard</NavLink>
+				</li>
+				<li>
+					<NavLink to="/login">Login</NavLink>
+				</li>
+				<li>
+					<NavLink to="/register">Register</NavLink>
+				</li>
+			</ul>
 
-export default Navigation
+			{token && <Button text="Logout" onClick={onLogout} />}
+		</nav>
+	);
+};
+
+export default Navigation;
