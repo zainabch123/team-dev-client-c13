be89952a6d67536ccfc0f29e6efcdcfc1f84c41c
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-21 00:57:18 +0800

    setup steps component

---
+++ src/components/steps/index.js
@@ -0,0 +1,15 @@
+import "./steps.css";
+
+const Steps = ({ active = 1 }) => {
+	return (
+		<ul className="steps">
+			{[...Array(4)].map((step, i) => (
+				<li className={`steps-number ${active === i + 1 && "active"}`} key={i}>
+					<span>{i + 1}</span>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default Steps;
--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,9 +1,20 @@
+import Steps from "../../components/steps";
 import "./welcome.css";
 
 const Welcome = () => {
 	return (
-		<div className="">
-			<div className="bg-white verify-card">welcome</div>
+		<div className="welcome bg-lightgrey">
+			<h1 className="h2">Welcome to Cohort Manager</h1>
+			<p className="text-lightblue">Create your profile to get started</p>
+			<div className="">
+				<h2>Create profile</h2>
+				<p className="text-lightblue">
+					Tell us about yourself to create your profile
+				</p>
+			</div>
+			<div className="">
+				<Steps />
+			</div>
 		</div>
 	);
 };
