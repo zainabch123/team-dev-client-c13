72c5cf1104a01ca34c5a29a075029ea591d44312
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-20 23:33:27 +0800

    setup welcome page and route

---
+++ src/pages/welcome/index.js
@@ -0,0 +1,11 @@
+import "./welcome.css";
+
+const Welcome = () => {
+	return (
+		<div className="">
+			<div className="bg-white verify-card">welcome</div>
+		</div>
+	);
+};
+
+export default Welcome;
