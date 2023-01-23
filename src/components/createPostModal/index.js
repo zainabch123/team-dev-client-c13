3a3f4ea00f082225052dc6f9fd2b2b582f4a2eec
Nathan King <nathanking3590@gmail.com>
2023-01-23 12:07:48 +0000

    add modal button styling

--- src/components/createPostModal/index.js
+++ src/components/createPostModal/index.js
@@ -2,10 +2,16 @@ import { useState } from "react"
 import useModal from "../../hooks/useModal"
 import Modal from "../modal"
 import './style.css'
+import Button from '../button'
 
 const CreatePostModal = () => {
     const { closeModal } = useModal()
     const [message, setMessage] = useState(null)
+    const [text, setText] = useState('')
+
+    const onChange = (e) => {
+        setText(e.target.value)
+    }
 
     const onSubmit = () => {
         setMessage('Submit button was clicked! Closing modal in 2 seconds...')
@@ -24,10 +30,18 @@ const CreatePostModal = () => {
             </section>
 
             <section>
-                <textarea placeholder="What's on your mind?"></textarea>
+                <textarea onChange={onChange} value={text} placeholder="What's on your mind?"></textarea>
             </section>
 
-            <button onClick={onSubmit}>Post</button>
+            <section className="create-post-actions">
+                <Button
+                    onClick={onSubmit}
+                    text='Post'
+                    classes={`${text.length > 0 ? 'blue' : 'offwhite' } width-full`}
+                    disabled={!text.length}
+                />
+            </section>
+
             {message && <p>{message}</p>}
         </Modal>
     )
