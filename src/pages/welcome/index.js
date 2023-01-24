016472fd3900bc550ff4625bae1c4bd6c12add36
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-25 02:43:18 +0800

    adding welcome second step, another global class

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,6 +1,8 @@
 import Steps from "../../components/steps";
 import Card from "../../components/card";
 import ProfileIcon from "../../assets/icons/profileIcon";
+import TextInput from "../../components/form/textInput";
+import Button from "../../components/button";
 import "./welcome.css";
 
 const Welcome = () => {
@@ -33,6 +35,58 @@ const Welcome = () => {
 						<p className="welcome-form-profileimg-error">
 							Please upload a valid image file
 						</p>
+					</div>
+					<div className="welcome-form-inputs">
+						<TextInput value="" name="firstName" label={"First name *"} />
+						<TextInput value="" name="lastName" label={"Last name *"} />
+						<TextInput value="" name="username" label={"Username *"} />
+						<TextInput
+							value=""
+							name="githubUsername"
+							label={"Github Username *"}
+						/>
+						<p className="text-blue1">*Required</p>
+					</div>
+					<div className="welcome-form-buttons">
+						<Button text="Back" classes="offwhite visibility-none" />
+						<Button text="Next" classes="blue" />
+					</div>
+				</form>
+			</Card>
+			<div className="welcome-form-popup-wrapper">
+				<Card>
+					<div className="welcome-form-popup">
+						<h4>Upload photo</h4>
+						<p>Choose a file to upload for your headshot.</p>
+						<div className="welcome-form-popup-buttons">
+							<Button text="Cancel" classes="offwhite" />
+							<Button text="Choose file" classes="blue" />
+						</div>
+					</div>
+				</Card>
+			</div>
+			<Card>
+				<div className="welcome-cardheader">
+					<h2>Create profile</h2>
+					<p className="text-blue1">
+						Tell us about yourself to create your profile
+					</p>
+				</div>
+				<div className="welcome-steps">
+					<Steps active={2} />
+				</div>
+				<div className="welcome-formheader">
+					<h3>Contact info</h3>
+				</div>
+				<form className="welcome-form">
+					<div className="welcome-form-inputs">
+						<TextInput value="" name="email" label={"Email *"} />
+						<TextInput value="" name="mobile" label={"Mobile *"} />
+						<TextInput name="password" label={"Password *"} type={"password"} />
+					</div>
+					<div className="welcome-form-buttons">
+						<Button text="Back" classes="offwhite" />
+						<Button text="Next" classes="blue" />
 					</div>
 				</form>
 			</Card>
