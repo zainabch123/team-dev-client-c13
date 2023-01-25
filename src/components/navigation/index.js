e38eabcc07b4bdc0da74a8548cb63704100a5cb2
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-26 01:18:22 +0800

    tidying css, using more global files

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,9 +1,8 @@
-import { NavLink } from "react-router-dom"
-import CohortIcon from "../../assets/icons/cohortIcon"
-import HomeIcon from "../../assets/icons/homeIcon"
-import ProfileIcon from "../../assets/icons/profileIcon"
-import useAuth from "../../hooks/useAuth"
-import './style.css'
+import { NavLink } from "react-router-dom";
+import CohortIcon from "../../assets/icons/cohortIcon";
+import HomeIcon from "../../assets/icons/homeIcon";
+import ProfileIcon from "../../assets/icons/profileIcon";
+import useAuth from "../../hooks/useAuth";
 
 const Navigation = () => {
 	const { token } = useAuth();
@@ -12,15 +11,30 @@ const Navigation = () => {
 		return null;
 	}
 
-    return (
-        <nav>
-            <ul>
-                <li><NavLink to='/'><HomeIcon colour="#000046" /><p>Home</p></NavLink></li>
-                <li><NavLink to='/'><ProfileIcon /><p>Profile</p></NavLink></li>
-                <li><NavLink to='/'><CohortIcon /><p>Cohort</p></NavLink></li>
-            </ul>
-        </nav>
-    )
-}
+	return (
+		<nav>
+			<ul>
+				<li>
+					<NavLink to="/">
+						<HomeIcon colour="#000046" />
+						<p>Home</p>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/">
+						<ProfileIcon />
+						<p>Profile</p>
+					</NavLink>
+				</li>
+				<li>
+					<NavLink to="/">
+						<CohortIcon />
+						<p>Cohort</p>
+					</NavLink>
+				</li>
+			</ul>
+		</nav>
+	);
+};
 
 export default Navigation;
