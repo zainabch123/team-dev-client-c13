fb1ebb7fdd4f9292de55a9d45d5e670ae535cc41
Chris Law <chris_law9208@hotmail.co.uk>
2023-01-26 01:28:32 +0800

    Revert "tidying css, using more global files"
    This reverts commit e38eabcc07b4bdc0da74a8548cb63704100a5cb2.

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,8 +1,9 @@
-import { NavLink } from "react-router-dom";
-import CohortIcon from "../../assets/icons/cohortIcon";
-import HomeIcon from "../../assets/icons/homeIcon";
-import ProfileIcon from "../../assets/icons/profileIcon";
-import useAuth from "../../hooks/useAuth";
+import { NavLink } from "react-router-dom"
+import CohortIcon from "../../assets/icons/cohortIcon"
+import HomeIcon from "../../assets/icons/homeIcon"
+import ProfileIcon from "../../assets/icons/profileIcon"
+import useAuth from "../../hooks/useAuth"
+import './style.css'
 
 const Navigation = () => {
 	const { token } = useAuth();
@@ -11,30 +12,15 @@ const Navigation = () => {
 		return null;
 	}
 
-	return (
-		<nav>
-			<ul>
-				<li>
-					<NavLink to="/">
-						<HomeIcon colour="#000046" />
-						<p>Home</p>
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/">
-						<ProfileIcon />
-						<p>Profile</p>
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to="/">
-						<CohortIcon />
-						<p>Cohort</p>
-					</NavLink>
-				</li>
-			</ul>
-		</nav>
-	);
-};
+    return (
+        <nav>
+            <ul>
+                <li><NavLink to='/'><HomeIcon colour="#000046" /><p>Home</p></NavLink></li>
+                <li><NavLink to='/'><ProfileIcon /><p>Profile</p></NavLink></li>
+                <li><NavLink to='/'><CohortIcon /><p>Cohort</p></NavLink></li>
+            </ul>
+        </nav>
+    )
+}
 
 export default Navigation;
