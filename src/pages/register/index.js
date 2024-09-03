8b06fe2af1b0aadbb7ce1ce4ee7bcb4899a66525
Atakan Temizkan <atikotemizkan@gmail.com>
2024-09-03 19:50:28 +0300

    added check for password length, added div for error message, added error message state

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -6,49 +6,58 @@ import CredentialsCard from "../../components/credentials";
 import "./register.css";
 
 const Register = () => {
-	const { onRegister } = useAuth();
-	const [formData, setFormData] = useState({ email: "", password: "" });
+  const { onRegister } = useAuth();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [errorMsg, setErrorMsg] = useState("");
 
-	const onChange = (e) => {
-		const { name, value } = e.target;
-		setFormData({ ...formData, [name]: value });
-	};
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
-	return (
-		<div className="bg-blue register credentialpage">
-			<CredentialsCard
-				title="Register"
-				socialLinksTitle="Or sign up with"
-				altButtonTitle="Already a user?"
-				altButtonLink="/login"
-				altButtonText="Log in"
-			>
-				<div className="register-form">
-					<form>
-						<TextInput
-							value={formData.email}
-							onChange={onChange}
-                            type="email"
-							name="email"
-							label={"Email *"}
-						/>
-						<TextInput
-							value={formData.password}
-							onChange={onChange}
-							name="password"
-							label={"Password *"}
-							type={"password"}
-						/>
-					</form>
-					<Button
-						text="Sign up"
-						onClick={() => onRegister(formData.email, formData.password)}
-						classes="green width-full"
-					/>
-				</div>
-			</CredentialsCard>
-		</div>
-	);
+  return (
+    <div className="bg-blue register credentialpage">
+      <CredentialsCard
+        title="Register"
+        socialLinksTitle="Or sign up with"
+        altButtonTitle="Already a user?"
+        altButtonLink="/login"
+        altButtonText="Log in"
+      >
+        <div className="register-form">
+          <form>
+            <TextInput
+              value={formData.email}
+              onChange={onChange}
+              type="email"
+              name="email"
+              label={"Email *"}
+            />
+            <TextInput
+              value={formData.password}
+              onChange={onChange}
+              name="password"
+              label={"Password *"}
+              type={"password"}
+            />
+          </form>
+          <div>{errorMsg}</div>
+          <Button
+            text="Sign up"
+            onClick={() => {
+              let enteredPass = formData.password;
+              if (enteredPass.length < 8) {
+                setErrorMsg("short");
+              } else {
+                onRegister(formData.email, formData.password);
+              }
+            }}
+            classes="green width-full"
+          />
+        </div>
+      </CredentialsCard>
+    </div>
+  );
 };
 
 export default Register;
