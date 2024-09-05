985f7a41db531a241884c80b249b1f12ee2478aa
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-09-05 14:09:10 +0100

    Removed console logs

--- src/pages/verification/index.js
+++ src/pages/verification/index.js
@@ -6,10 +6,7 @@ import { useNavigate } from "react-router-dom";
 const Verification = () => {
     const navigate = useNavigate();
 
-	console.log("we are here")
-
     const onContinueClick = () => {
-		console.log("button clicked");
         navigate('/welcome')
     }
 
