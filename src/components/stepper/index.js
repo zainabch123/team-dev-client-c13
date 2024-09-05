f79658716dbcdeb24fe5af88378b83d2d261abd2
Thomas Jensen <thomas.jensen_@outlook.com>
2024-09-05 15:36:28 +0100

    Added textfield validation for firstName and lastName

--- src/components/stepper/index.js
+++ src/components/stepper/index.js
@@ -4,7 +4,7 @@ import Button from "../button";
 import "./style.css";
 import { useState } from "react";
 
-const Stepper = ({ header, children, onComplete }) => {
+const Stepper = ({ data, header, children, onComplete }) => {
     const [currentStep, setCurrentStep] = useState(0)
 
     const onBackClick = () => {
@@ -22,21 +22,28 @@ const Stepper = ({ header, children, onComplete }) => {
         setCurrentStep(currentStep+1)
     }
 
+    const validateInput = () => {
+        if (data.firstName || data.lastName) {
+            onNextClick()
+        }
+    }
+
 	return (
         <Card>
             {header}
             <div className="steps-container">
-                <Steps maxSteps={children.length} currentStep={currentStep} />
+                <Steps maxSteps={children.length} currentStep={currentStep}/>
             </div>
 
             {children[currentStep]}
 
             <div className="stepper-buttons">
-                <Button text="Back" classes="offwhite" onClick={onBackClick} />
-                <Button text={currentStep === children.length-1 ? 'Submit' : 'Next'} classes="blue" onClick={onNextClick} />
+                <Button text="Back" classes="offwhite" onClick={onBackClick}/>
+                <Button text={currentStep === children.length - 1 ? 'Submit' : 'Next'} classes="blue"
+                        onClick={validateInput} />
             </div>
         </Card>
-	);
+    );
 };
 
 export default Stepper;
