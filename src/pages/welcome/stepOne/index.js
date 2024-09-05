c2f78e0ff989752dbc4c2cd734ad9df4f74cbe05
Thomas Jensen <thomas.jensen_@outlook.com>
2024-09-05 15:37:13 +0100

    Finished base64 conversion function

--- src/pages/welcome/stepOne/index.js
+++ src/pages/welcome/stepOne/index.js
@@ -6,15 +6,24 @@ import {useState} from "react";
 const StepOne = ({ data, setData }) => {
     const [profilePicture, setProfilePicture] = useState("")
 
-    const convertToBase64 = (event) => {
+    const toBase64 = (event) => {
         const file = event.target.files[0]
+        let base64
+
         if (file) {
             const reader = new FileReader()
-            reader.onload = () => {
-                setProfilePicture(reader.result)
-                setData(prevData => ({ ...prevData, profilePicture: reader.result }))
-            }
             reader.readAsDataURL(file)
+            reader.onload = () => {
+                // Restructuring reader output to mimic event
+                base64 = reader.result.toString()
+                setProfilePicture(base64)
+                setData(event = {
+                    target: {
+                        name: "profilePicture",
+                        value: base64,
+                    }
+                })
+            }
         }
     }
 
@@ -28,28 +37,26 @@ const StepOne = ({ data, setData }) => {
                     <p className="text-blue1">Photo</p>
                     <div className="welcome-form-profileimg-input">
                         {profilePicture ? (
-                            <img src={profilePicture} alt="Avatar Preview" style={{ width: '100px', height: '100px', objectFit: 'cover' }} />
+                            <img src={profilePicture} alt="Avatar Preview" style={{ width: '50px', height: '50px', objectFit: 'cover' }} />
                         ) : (
                             <ProfileIcon colour="#28C846" background="#64DC78" />
                         )}
-                        {/*  TODO: Preview file on upload  */}
-                        <input type="file" accept="image/png, image/jpeg" onChange={convertToBase64} />
+                        <input type="file" name="profilePicture" accept="image/png, image/jpeg" onChange={toBase64} />
                     </div>
                     <p className="welcome-form-profileimg-error">
                         Please upload a valid image file
                     </p>
                 </div>
                 <div className="welcome-form-inputs">
-                    <TextInput onChange={setData} value={data.firstName} name="firstName" label={"First name"} />
-                    {/*  TODO: Add required attribute to these  */}
-                    <TextInput onChange={setData} value={data.lastName} name="lastName" label={"Last name"} />
+                    <TextInput onChange={setData} value={data.firstName} name="firstName" label={"First name*"} />
+                    <TextInput onChange={setData} value={data.lastName} name="lastName" label={"Last name*"} />
                     <TextInput
                         onChange={setData}
                         value={data.githubUsername}
                         name="githubUsername"
                         label={"Github Username"}
                     />
-                    <p className="text-blue1">*Required</p>
+                    <p className="text-blue1">*required</p>
                 </div>
             </Form>
         </>
