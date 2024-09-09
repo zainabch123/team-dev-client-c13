eb36504e8caa009487c19dee73690302b85e789f
Zainab Choudhry <zainabchoudhry05@gmail.com>
2024-09-09 13:11:55 +0100

    Added getUser function to get user by id

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,11 +1,24 @@
 import useAuth from "../../hooks/useAuth";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { getUser } from "../../service/apiClient";
 import "./profile.css";
 
 const Profile = () => {
   const { user } = useAuth();
-  
+  const { id } = useParams();
+  const [userProfile, setUserProfile] = useState(null);
+
+  useEffect(() => {
+    getUser(id).then(setUserProfile);
+  }, [id]);
+
+
+  console.log("id", id);
+  console.log("selected User", userProfile);
+
   // console.log("user from profile", user);
-  
+
   return <h1>Welcome to the Profile page!</h1>;
 };
 
--- src/service/apiClient.js
+++ src/service/apiClient.js
@@ -1,59 +1,64 @@
-import { API_URL } from "./constants"
+import { API_URL } from "./constants";
 
 async function login(email, password) {
-    return await post('login', { email, password }, false)
+  return await post("login", { email, password }, false);
 }
 
 async function register(email, password) {
-    await post('users', { email, password }, false)
-    return await login(email, password)
+  await post("users", { email, password }, false);
+  return await login(email, password);
 }
 
 async function createProfile(userId, firstName, lastName, githubUrl, bio) {
-    return await patch(`users/${userId}`, { firstName, lastName, githubUrl, bio })
+  return await patch(`users/${userId}`, {
+    firstName,
+    lastName,
+    githubUrl,
+    bio,
+  });
 }
 
 async function getPosts() {
-    const res = await get('posts')
-    return res.data.posts
+  const res = await get("posts");
+  return res.data.posts;
+}
+
+async function getUser(userId) {
+  const res = await get(`users/${userId}`);
+  return res.data.user;
 }
 
 async function post(endpoint, data, auth = true) {
-    return await request('POST', endpoint, data, auth)
+  return await request("POST", endpoint, data, auth);
 }
 
 async function patch(endpoint, data, auth = true) {
-    return await request('PATCH', endpoint, data, auth)
+  return await request("PATCH", endpoint, data, auth);
 }
 
 async function get(endpoint, auth = true) {
-    return await request('GET', endpoint, null, auth)
+  return await request("GET", endpoint, null, auth);
 }
 
 async function request(method, endpoint, data, auth = true) {
-    const opts = {
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        method
-    }
+  const opts = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    method,
+  };
 
-    if (method.toUpperCase() !== 'GET') {
-        opts.body = JSON.stringify(data)
-    }
+  if (method.toUpperCase() !== "GET") {
+    opts.body = JSON.stringify(data);
+  }
 
-    if (auth) {
-        opts.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`
-    }
+  if (auth) {
+    opts.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
+  }
 
-    const response = await fetch(`${API_URL}/${endpoint}`, opts)
+  const response = await fetch(`${API_URL}/${endpoint}`, opts);
 
-    return response.json()
+  return response.json();
 }
 
-export {
-    login,
-    getPosts,
-    register,
-    createProfile
-}
+export { login, getPosts, getUser, register, createProfile };
