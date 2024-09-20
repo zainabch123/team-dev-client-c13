a7d7fb0beb413d8336088bb684a054c053d5a15f
Atakan Temizkan <atikotemizkan@gmail.com>
2024-09-20 12:09:42 +0300

    added error message to display valid special characters and implemented displaying multiple errors at one time

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -6,11 +6,11 @@ export const validateEmail = (email) => {
   }
 
   if (!email.includes("@")) {
-    return "Email must contain '@'.";
+    return "Email must contain '@'";
   }
 
   if (!email.includes(".")) {
-    return "Email must contain a '.'.";
+    return "Email must contain a '.'";
   }
 
   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
@@ -23,23 +23,38 @@ export const validateEmail = (email) => {
 
 // Function to validate password with conditions
 export const validatePassword = (password) => {
-  const specialChars = /^(?=.*[!@#$%^&*])/;
+  const specialChars = /^(?=.*[!@#$_~\-%^&*])/;
   const numbers = /^(?=.*[0-9])/;
-  const forbiddenChars = /^(?=.*[(){}[\]|`¬¦"'<>:;|~_\-+=,])/;
+  const forbiddenChars = /^(?=.*[(){}[\]|`¬¦"'<>:;|+=,])/;
+  const passwordErrors = [];
 
   if (forbiddenChars.test(password)) {
-    return "Password contains invalid characters!";
-  } else if (password.length < 8) {
-    return "Password must be at least 8 characters long!";
-  } else if (
+    passwordErrors.push("Password contains invalid characters!", <br />);
+  }
+  if (password.length < 8) {
+    passwordErrors.push("Password must be at least 8 characters long!", <br />);
+  }
+  if (
     password.toLowerCase() === password ||
     password.toUpperCase() === password
   ) {
-    return "Password must contain at least 1 uppercase and 1 lowercase letter.";
-  } else if (!numbers.test(password)) {
-    return "Password must contain at least 1 number.";
-  } else if (!specialChars.test(password)) {
-    return "Password must contain at least 1 special character.";
+    passwordErrors.push(
+      "Password must contain at least 1 uppercase and 1 lowercase letter.",
+      <br />
+    );
+  }
+  if (!numbers.test(password)) {
+    passwordErrors.push("Password must contain at least 1 number.", <br />);
+  }
+  if (!specialChars.test(password)) {
+    passwordErrors.push(
+      "Password must contain at least one of the following: !@#$-_~%^&*",
+      <br />
+    );
+  }
+  if (passwordErrors[0]) {
+    console.log(passwordErrors);
+    return passwordErrors;
   } else {
     return "";
   }
